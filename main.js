4638716b10829d8b21772747c76d27bc0c19f4ba
Lao <laslolanus@gmail.com>
2024-11-27 12:26:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.height = 1024
+canvas.width = 768
+
+// Bird variables
+
+function drawBird () {
+
+}
+function draw () {
+    requestAnimationFrame(draw)
+}
